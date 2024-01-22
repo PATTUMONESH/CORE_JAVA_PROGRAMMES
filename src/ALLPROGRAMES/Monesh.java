feb3534b4cfe468c2745aee44d216dbd2c6aebe2
Massil <pattumonesh@gmail.com>
2024-01-22 19:07:42 +0530

    javaprograms

---
+++ src/ALLPROGRAMES/Monesh.java
@@ -0,0 +1,19 @@
+package ALLPROGRAMES;
+
+
+public class Monesh{
+	
+	
+	public static void main(String args[]) {
+		
+		
+		int d=10;
+		String name="purnima";
+		
+		System.out.println(d);	
+		System.out.println(name);
+		
+	}
+	
+		
+}
